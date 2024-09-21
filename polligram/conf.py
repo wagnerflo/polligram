5c3af510e3007895144bfaf8aa058dd3983e714d
Florian Wagner <florian@wagner-flo.net>
2024-09-21 21:55:08 +0200

    Graciously handle errors reloading configuration file.

--- polligram/conf.py
+++ polligram/conf.py
@@ -1,15 +1,27 @@
 from collections.abc import Mapping
+from logging import getLogger
 from pathlib import Path
-from tomllib import load as decode_toml
+from tomllib import load as decode_toml, TOMLDecodeError
+
+logger = getLogger("polligram")
 
 class Config(Mapping):
     def __init__(self):
+        self._data = None
         self.path = Path(f"{__package__}.toml").resolve()
         self.reload()
 
     def reload(self):
         with open(self.path, "rb") as fp:
-            self._data = decode_toml(fp)
+            try:
+                self._data = decode_toml(fp)
+            except TOMLDecodeError:
+                if self._data is None:
+                    raise
+                logger.warning(
+                    "Couldn't reload configuration file.",
+                    exc_info=True,
+                )
 
     def __getitem__(self, key):
         return self._data[key]
